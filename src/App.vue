0c228895cf125d67da56f9acaaf12944a51927f6
fazla23 <fazlarabbi366iou@gmail.com>
2020-04-28 02:51:14 +0600

    'Finished'

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,78 @@
 <template>
   <div id="app">
-    <Todos />
+    <Header />
+    <br>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <br>
+    <h2>Following are todos:</h2>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
+
   },
   data(){                 //data is a function that returns an object
     return {
       name:'todo List',
       todos:[             //todos is an element of the object that return data function
                           //todos value is an array of object(list of the todo items)
-        {
-          id:1,
-          title:"Todo One",
-          completed: false
-        },
-        {
-          id:2,
-          title:"Todo Two",
-          completed: true
-        },
-        {
-          id:3,
-          title:"Todo Three",
-          completed: false
-        }
+        // {
+        //   id:1,
+        //   title:"Todo One",
+        //   completed: false
+        // },
+        // {
+        //   id:2,
+        //   title:"Todo Two",
+        //   completed: true
+        // },
+        // {
+        //   id:3,
+        //   title:"Todo Three",
+        //   completed: false
+        // }
         
       ]
     }
+  },
+  methods:{
+        deleteTodo(id){
+          axios.delete('https://jsonplaceholder.typicode.com/todos/'+id)
+  .then(this.todos = this.todos.filter(todo=>todo.id!==id))
+  .catch(err=>console.log(err));
+
+
+          //this.todos = this.todos.filter(todo=>todo.id!==id); //without axios
+        },
+
+        addTodo(newTodo){
+          const {id,title,completed}=newTodo; //const means construct
+          //this.todos = [...this.todos, newTodo]; //it works without the axios
+          axios.post('https://jsonplaceholder.typicode.com/todos',{
+            id,
+            title,
+            completed
+          })
+            .then(res => this.todos=[...this.todos, res.data])
+            .catch(err=>console.log(err));
+      }
+        
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .then(res => this.todos=res.data)
+  .catch(err=>console.log(err));
   }
 }
 </script>
@@ -48,5 +87,36 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.5;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: green;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.btn:hover {
+  background: #666;
+}
+
+#app{
+  max-width: 600px;
+  margin: auto;
+  border: 2px solid #666;
+  border-radius: 3px;
+  padding: 3px;
+}
+
+.btn1 {
+  display: inline-block;
+  border: 2px;
+  background: #bbb;
+  color: black;
+  padding: 7px 20px;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <p> <b>Add Todo item:</b> <input type="text" v-model="title" name="title" placeholder="Add todo...." class="btn1"></p>
+            <input type="submit" value="Submit" class="btn btn-success ">
+        </form>
+    </div>
+</template>
+
+<script>
+//import { v4 as uuidv4 } from 'uuid';   //this line is used for generating a random id
+export default {
+    name:"AddTodo",
+    data(){
+        return {
+            title:''
+        }
+    },
+    methods: {
+
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                id:this.id,  ////this line is used for generating a random id
+                title:this.title,
+                completed:false
+            }
+            //Send up to parent
+            this.$emit('add-todo',newTodo);
+            this.title="";
+        },
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    
+
+
+
+
+</style>
